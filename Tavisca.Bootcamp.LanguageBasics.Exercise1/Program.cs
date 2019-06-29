1c7e1291cf8adac00364b76bd0ecb6eae1aa924f
tavisca-akhatri <akhatri@tavisca.in>
2019-06-29 21:36:00 +0530

    Assignment 1 changes made

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,73 +23,46 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            int length = equation.Length;
-            int result = 0;
-            char[] equationch = equation.ToCharArray();   //converting string to character array
-            for(int i = 0 ; i < length ; i++){
-                if(equationch[i] == '?' && i >= (length/2 + 1)){  //checking if ? is after = sign
-                    string[] splitonequal = equation.Split("="); //split equation on equal sign to get 2 strings
-                    string charsbeforeequal = splitonequal[0];  //contains characters which are before equal
-                    string charsafterequal = splitonequal[1];   //contains characters after equal
-                    string[] splitonmul = charsbeforeequal.Split("*"); //split on multiplication
-                    string number1 = splitonmul[0];  // first number
-                    string number2 = splitonmul[1];  // second number
-                    int num1 = Int32.Parse(number1);
-                    int num2 = Int32.Parse(number2);
-                    int resultmul = num1 * num2;     //getting number   
-                    string result1 = resultmul.ToString();
-                    char[] chwithquestionmark = charsafterequal.ToCharArray();  //contain equal sign  
-                    char[] chwithnumbers = result1.ToCharArray(); //contain result
-                    char resultch = ' ';
-                    for(int j = 0 ; j < charsafterequal.Length ; j++){//comparing both result and result with ? mark
-                        if(chwithquestionmark[j] == '?'){
-                            resultch = chwithnumbers[j];
-                        }
-                    }
-                    result = resultch - '0';  //subtracting to get int
-                    return result;
-                }else if(equationch[i] == '?' && i < (length/2 + 1)){// ? on left side of equation
-                    string[] splitonequal = equation.Split("="); //splitting on = to get 2 strings
-                    string charsbeforeequal = splitonequal[0]; // string with = sign
-                    string charsafterequal = splitonequal[1]; //string without original sign
-                    string[] splitonmul = charsbeforeequal.Split("*"); //split on * to get the two numbers
-                    string str3 = splitonmul[0]; // first number
-                    string str4 = splitonmul[1]; // second number
-                    int num1 = 0;
-                    int num2 = 0;
-                    int num3 = Int32.Parse(charsafterequal); //converting result to int
-                    int resultdiv = 0;
-                    if(str3.Contains('?')){
-                        num2 = Int32.Parse(str4); //if first num has ? mark
-                        resultdiv = num3 / num2; //divide result with 2nd number
-                    }else{
-                        if(str4.Contains('?') && str4.Length == 3){
-                            result = -1; 
-                            break;
-                        }
-                        num1 = Int32.Parse(str3); //if second number has ? mark
-                        resultdiv = num3 / num1;  //divide result with 1st number
-                    }
-                    string result1 = resultdiv.ToString();  
-                    char[] ch1 = new char[]{};  //searching for question mark in the number
-                    if(str3.Contains('?')){
-                        ch1 = str3.ToCharArray();
-                    }else{
-                        ch1 = str4.ToCharArray();
-                    }
-                    char[] ch2 = result1.ToCharArray(); //number found by dividing
-                    char resultch = ' ';
-                    for(int j = 0 ; j < ch1.Length ; j++){ //searching ? and getting appropriate number
-                        if(ch1[j] == '?'){
-                            resultch = ch2[j];
-                        }
-                    }
-                    result = resultch - '0';
-                    return result;
+            int index_star = equation.IndexOf("*");
+            int index_equal = equation.IndexOf("=");
+            String operand1 = equation.Substring(0, index_star);
+            String operand2 = equation.Substring(index_star+1, index_equal-index_star-1);
+            String operand3 = equation.Substring(index_equal+1);
+
+            if(operand1.IndexOf("?") != -1 || operand2.IndexOf("?") != -1)
+            {
+                if(operand2.IndexOf("?") != -1)
+                {
+                    String temp = operand1;
+                    operand1 = operand2;
+                    operand2 = temp;
                 }
+                int val3 = int.Parse(operand3);
+                int val2 = int.Parse(operand2);
+                int val1 = val3 / val2;
+                String operand1original = val1.ToString();
+                if(operand1.Length != operand1original.Length)
+                {
+                    return -1;
+                }
+                int index = operand1.IndexOf("?");
+                char result = operand1original[index];
+                return result - '0' == 0 && index == 0 ? -1 : (result-'0');
             }
-            return result;
+            else
+            {
+                int val1 = int.Parse(operand1);
+                int val2 = int.Parse(operand2);
+                int val3 = val1 * val2;
+                String operand3original = val3.ToString();
+                if(operand3.Length != operand3original.Length)
+                {
+                    return -1;
+                } 
+                int index = operand3.IndexOf("?");
+                char result = operand3original[index];
+                return result - '0' == 0 && index == 0 ? -1 : (result - '0');
+            }
         }
     }
 }

df3f6f41f4d1d939230a8dddc2617006707b5208
tavisca-akhatri <akhatri@tavisca.com>
2019-08-07 11:27:55 +0530

    C# implementation converted into Java implementation with solid principles and clean code practice

---
+++ src/test/java/FixMultiplicationTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FixMultiplicationTest {
+    @Test
+    public void checkFixMultiplication(){
+        assertEquals(9,new FixMultiplication().findDigit("42*47=1?74"));
+        assertEquals(2,new FixMultiplication().findDigit("4?*47=1974"));
+        assertEquals(4,new FixMultiplication().findDigit("42*?7=1974"));
+        assertEquals(-1,new FixMultiplication().findDigit("42*?47=1974"));
+       // assertEquals(-1,new FixMultiplication().findDigit("2*12?=247"));
+    }
+}
